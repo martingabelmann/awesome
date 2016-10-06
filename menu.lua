d929d1d0844643d4f7172e20faa79a576613248d
Martin Gabelmann <martin@gabelmann.biz>
2016-10-06 17:55:18 +0200

    shutdown dialog with naughty

--- menu.lua
+++ menu.lua
@@ -1,13 +1,48 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
+local naughty = require("naughty")
+
+function askfor(question, func) 
+	naughty.notify({
+        title   = question,
+        text    = "click for 'yes'",
+	    timeout = 5,
+	    run     = func
+	})
+end
 
 mymainmenu = awful.menu({ 
     items = { 
-        { "logout", awesome.quit },
-        { "poweroff", terminal .. " -x systemctl poweroff -i" },
-        { "reboot", terminal .. " -x systemctl reboot -i"},
-        { "standby", terminal .. " -x systemctl suspend" },
+        { "logout", function() askfor(
+            "Do you really want to logout?",  
+            awesome.quit
+            ) 
+            end 
+        },
+        
+        { "poweroff", function() askfor(
+            "Do you really want to shutdown?",  
+            function() awful.util.pread(terminal .. " -x systemctl poweroff -i") end
+            ) 
+            end 
+        },
+
+        { "reboot", function() askfor(
+            "Do you really want to reboot?",  
+            function() awful.util.pread(terminal .. " -x systemctl reboot -i") end
+            ) 
+            end 
+        },
+
+        { "standby", function() askfor(
+            "Do you really want to suspend?",  
+            function() awful.util.pread(terminal .. " -x systemctl suspend") end
+            ) 
+            end 
+        },
+        
         { "lock screen", terminal .. " -x slock" },
+        
         { "awesome", nil, theme.awesomearch_icon }
     }
 })
